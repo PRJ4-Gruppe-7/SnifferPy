fe675915ac507cde2473bad7c30359fffb09ebf2
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-04-21 17:45:26 +0200

    Added multiple signal values

--- WiFi_sniffer.py
+++ WiFi_sniffer.py
@@ -5,18 +5,23 @@ from scapy.all import *
 mac = MacLookup()
 
 conf.iface = "mon0"
-devices = set()
+devices = {}
 ## Define our Custom Action function
 def custom_action(packet):
     if packet.haslayer(Dot11):
-        if packet.addr2 and (packet.addr2 not in devices) :
-            devices.add(packet.addr2)
+        if packet.addr2 and str(packet.addr2) not in devices :
+            devices[str(packet.addr2)] = [packet.dBm_AntSignal]
+
             try:
                 device = mac.lookup(packet.addr2)
             except:
                 device = str(packet.addr2)
 
-            print(len(devices), device,str(packet.dBm_AntSignal) + "dBm")
+            print(len(devices), device ,str(packet.dBm_AntSignal) + "dBm")
+
+        else:
+            devices[str(packet.addr2)] += [packet.dBm_AntSignal]
+            print(mac.lookup(packet.addr2),devices[str(packet.addr2)])
 
 
 ## Setup sniff
